0f9bf7a3c275860b80d365a31bb5b25152a72eb0
PascalPlantey <59055046+PascalPlantey@users.noreply.github.com>
2023-08-31 17:13:26 +0200

    Corrections to the JSDoc

--- src/hooks/browser/useDocumentVisibility.js
+++ src/hooks/browser/useDocumentVisibility.js
@@ -1,21 +1,35 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 import useEventListener from "../elements/useEventListener";
 
-const getVisibility = () => ({ visible: document?.visibilityState === 'visible' });
+const getVisibility = () => document?.visibilityState === 'visible';
 
 /**
  * Tracks visibility of the browser window
  * @param {boolean} immediately Start tracking immedialtely or when toggle is called?
  * @param {object} [options={}] { capture, once, passive } [see Mozilla]{@link https://developer.mozilla.org/fr/docs/Web/API/EventTarget/addEventListener}
- * @returns {object} { visibility: 'visible'|'hidden', working: boolean, toggle: function() {} }
+ * @returns {object} { visible: boolean, working: boolean, toggle: function() {} }
  * @memberof Hooks#
  */
 const useDocumentVisibility = (immediately = true, options = {}) => {
-  const [visibility, setVisibility] = useState(getVisibility);    // !!! Function initialization
-  const listener = useEventListener('visibilitychange', () => setVisibility(getVisibility()), document, immediately, options);
+  const [visible, setVisible] = useState(getVisibility);    // !!! Function initialization
+  const listener = useEventListener('visibilitychange', useCallback(() => setVisible(getVisibility()), []), document, immediately, options);
 
-  return({ ...visibility, ...listener });
+  return({ visible, ...listener });
 };
+
+/* for use with useCheckHook
+  const resultDesc = {
+    type: 'object',
+    values: [
+      { name: 'visible', type: 'boolean' },
+      { name: 'working', type: 'boolean' },
+      { name: 'toggle', type: 'function' }
+    ]
+  };
+  const res = useCheckHook('hook', useDocumentVisibility, resultDesc, step);
+ */
+
+const res = useCheckHook('hook', useDocumentVisibility, resultDesc, step);
 
 export default useDocumentVisibility;
