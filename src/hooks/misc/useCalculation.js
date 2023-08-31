189bd469f516de015126bde78d2dca762b41c722
PascalPlantey <59055046+PascalPlantey@users.noreply.github.com>
2023-08-31 19:32:15 +0200

    useCalculation, changed the useState with a function initialization

--- src/hooks/misc/useCalculation.js
+++ src/hooks/misc/useCalculation.js
@@ -3,12 +3,13 @@ import { useState, useEffect } from 'react';
 /**
  * Execute a function whenever it changes and returns its result. calculateFn should be a 'useCallback'
  * @param {function} calculateFn Function to execute () => any
- * @returns {any} calculateFn return value
- * @example const result = useCalculation(useCallback(() => param1 * param2, [param1, param2]));
+ * @returns {any} Value returned by calculateFn
+ * @example
+ * const result = useCalculation(useCallback(() => param1 * param2, [param1, param2]));
  * @memberof Hooks#
  */
 const useCalculation = calculateFn => {
-  const [result, setResult] = useState(calculateFn());
+  const [result, setResult] = useState(calculateFn);
 
   useEffect(() => {
     setResult(calculateFn());
