07c6393bc4538a79430e53d067e19a43aa23652a
PascalPlantey <59055046+PascalPlantey@users.noreply.github.com>
2023-09-01 12:18:30 +0200

    useGeolocation updated to sent less state updates

--- src/hooks/browser/useGeolocation.js
+++ src/hooks/browser/useGeolocation.js
@@ -13,15 +13,51 @@ import useInterval from '../timers/useInterval';
 const useGeolocation = (interval = 2000, immediately = true, options) => {
   const [result, setResult] = useState();
 
-  const getPosition = useCallback(() => navigator.geolocation.getCurrentPosition(setResult, setResult, options), [options]);
+  // Change only if needed to avoid unnecessary state update as getCurrentPosition always passes a new object
+  // State update if result type is updated, or if coordinates are updated, or if the error is updated
+  const handleResult = result => setResult(prevResult => {
+      let changes = false;
+
+      if ((prevResult instanceof GeolocationPosition && result instanceof GeolocationPosition) ||             // eslint-disable-line
+          (prevResult instanceof GeolocationPositionError && result instanceof GeolocationPositionError)) {   // eslint-disable-line
+
+        const prev = prevResult instanceof GeolocationPosition ? prevResult.coords : prevResult,              // eslint-disable-line
+              curr = prevResult instanceof GeolocationPosition ? result.coords : result;                      // eslint-disable-line
+
+        for(const key in prev)
+          if (!(key in curr) || prev[key] !== curr[key]) {
+            changes = true;
+            break;
+          }
+      }
+      else
+        changes = true;
+
+      return changes ? result : prevResult;
+  });
+
+  const getPosition = useCallback(() => navigator.geolocation.getCurrentPosition(handleResult, handleResult, options), [options]);
 
   const { working, toggle } = useInterval(getPosition, interval, immediately);
 
-  const success = result instanceof GeolocationPosition ? true :
-                  result instanceof GeolocationPositionError ? false :
-                  undefined;
+  const success = result instanceof GeolocationPosition ? true :                                              // eslint-disable-line
+                  result instanceof GeolocationPositionError ? false :                                        // eslint-disable-line
+                  false;
 
   return({ success, result, working, toggle });
 };
 
+/* Debug with --------------------------------------------------*
+ * const resultDesc = {
+ *   type: 'object',
+ *   values: [
+ *     { name: 'success', type: 'boolean' },
+ *     { name: 'result', type: 'object' },
+ *     { name: 'working', type: 'boolean' },
+ *     { name: 'toggle', type: 'function' }
+ *   ]
+ * };
+ *
+ * const res = useCheckHook('hook', useGeolocation, resultDesc);
+ */
 export default useGeolocation;
