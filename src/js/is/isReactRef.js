340469460fabd8be338a13c892be9e44b3bc478f
PascalPlantey <59055046+PascalPlantey@users.noreply.github.com>
2023-09-02 10:23:03 +0200

    JSDoc update

--- src/js/is/isReactRef.js
+++ src/js/is/isReactRef.js
@@ -2,6 +2,7 @@
  * Indicates if a variable is a react Ref (createRef, useRef, ...)
  * @param {any} variable Variable to be tested
  * @returns {boolean}
+ * @memberof JS_IsFunctions#
  */
 const isReactRef = variable => typeof variable === 'object' && 'current' in variable;
 
