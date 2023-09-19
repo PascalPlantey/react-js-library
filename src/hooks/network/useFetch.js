b58df44da676a4c590832976a22527bb07e97743
PascalPlantey <59055046+PascalPlantey@users.noreply.github.com>
2023-09-19 17:04:51 +0200

    added an abort function to useFetch

--- src/hooks/network/useFetch.js
+++ src/hooks/network/useFetch.js
@@ -1,6 +1,6 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
-import { useObject } from '../misc';
+import { useObject, useToggle } from '../misc';
 
 const initialObj = {                                                // Make it const to avoid unnecessary renders
   loading: true,
@@ -16,30 +16,39 @@ const initialObj = {                                                // Make it c
  * @param {string} [api=''] API end point ([Mozilla] {@link https://developer.mozilla.org/en-US/docs/Web/API/URL/URL})
  * @param {string} url Base url
  * @param {object} options Fetch options ([Mozilla]) {@link https://developer.mozilla.org/en-US/docs/Web/API/fetch}
- * @returns {useFetchResult} { status, ok, statusText, loading, result }
+ * @returns {Array} [{ loading, ok, status, statusText, data, error }, refresh, abort]
  * @memberof Hooks#
  */
 const useFetch = (api = '', url, options) => {
+  const refController = useRef();
+  const [, toggle] = useToggle();
   const { object, assign, reset } = useObject(initialObj);
+
+  if (!refController.current)
+    refController.current = new AbortController();
 
   useEffect(() => {
     reset();                                                        // Clear result to default initialObj
 
-    fetch(encodeURI(new URL(api, url)), options)
+    fetch(encodeURI(new URL(api, url)), { ...options, signal: refController.current?.signal })
     .then(result => {
       const { ok, status, statusText } = result;
 
       if (ok)
-        result.json()                                               // .then is here to have the result.status etc, .catch falls up to the one provided
+        result.json()                                               // .then is here to have the result.status etc, .catch falls down to the one below
         .then(data => assign({ loading: false, ok, status, statusText, data }));
       else
         assign({ loading: false, ok, status, statusText });         // No data available
     })
     .catch(error => assign({ loading: false, ok: false, error }));  // fetch or json error
 
-  }, [api, url, options, assign, reset]);
+    return () => {
+      refController.current?.abort();
+      refController.current = null;
+    }
+  }, [api, url, options, assign, reset, toggle]);
 
-  return object;
+  return [object, toggle, refController.current?.abort || noop];
 };
 
 export default useFetch;
