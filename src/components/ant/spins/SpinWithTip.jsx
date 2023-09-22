965c25bcf094b5663d5744c3354d2d7f83d53d32
PascalPlantey <59055046+PascalPlantey@users.noreply.github.com>
2023-09-22 19:10:09 +0200

    Added SpinWithTip

---
+++ src/components/ant/spins/SpinWithTip.jsx
@@ -0,0 +1,43 @@
+import React, { forwardRef } from "react";
+
+import { Spin, Typography } from "antd";
+import PropTypes from 'prop-types';
+
+import './spins.css';
+
+const propTypes = {
+  containerClassName: PropTypes.string,
+  containerStyle: PropTypes.object,
+  tip: PropTypes.string,
+  size: PropTypes.oneOf(['default', 'large', 'small']),
+  hidden: PropTypes.bool
+};
+
+/**
+ * @function SpinWithTip
+ * @param {string} [containerClassName] Container class name
+ * @param {object} [containerStyle] Extra style information for the container
+ * @param {boolean} [hidden=false] Hide the spin?
+ * @param {string} [tip] Tip text
+ * @param {string} [size='default'|'large'|'small'] Spin image size
+ * @param {any} [props] All other props are injected in the antd Spin component
+ * @returns {React.Component}
+ */
+const SpinWithTip = forwardRef((props, ref) => {
+  const { containerClassName, containerStyle, tip, hidden, ...spinProps } = props;
+
+  return(hidden ?
+    null
+    :
+    <div className={containerClassName} style={containerStyle} ref={ref}>
+      <Spin className='pp-spin-container-centered' size='' {...spinProps} />
+      <span className='pp-spin-container-centered'>
+        <Typography>{tip}</Typography>
+      </span>
+    </div>
+  );
+});
+
+SpinWithTip.propTypes = propTypes;
+
+export default SpinWithTip;
