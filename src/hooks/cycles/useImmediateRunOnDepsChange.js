789712ffbadef2dfa080eafbdbfa61b3d86d297b
PascalPlantey <59055046+PascalPlantey@users.noreply.github.com>
2023-10-02 10:21:00 +0200

    useImmediateRunOnDepsChange now uses useCheckValuesChanges

--- src/hooks/cycles/useImmediateRunOnDepsChange.js
+++ src/hooks/cycles/useImmediateRunOnDepsChange.js
@@ -1,6 +1,7 @@
 import { useRef } from "react";
 
 import { surfaceEquals } from "../../js/extensions";
+import { useCheckValuesChanges } from "../misc";
 
 /**
  * Execute a function immediately before render, and before any other render when a dependency value changes
@@ -9,15 +10,11 @@ import { surfaceEquals } from "../../js/extensions";
  * @returns {any} Value returned by fn
  */
 const useImmediateRunOnDepsChange = (fn, deps) => {
+  const [, checkChanges] = useCheckValuesChanges();
   const resultRef = useRef();
-  const depsRef = useRef([]);
-  const updateDepsRef = () => {
-    const changed = !surfaceEquals(depsRef.current, deps);
-    depsRef.current = deps;
-    return changed;
-  }
 
-  if (updateDepsRef()) resultRef.current = fn();
+  if (checkChanges(deps))
+    resultRef.current = fn();
 
   return resultRef.current;
 };
