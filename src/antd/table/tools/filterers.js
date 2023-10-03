34de7ad5a9ba79135b8371e6905ad3ae7762f72a
PascalPlantey <59055046+PascalPlantey@users.noreply.github.com>
2023-10-03 12:50:59 +0200

    Added onArrayFilter in filterers

--- src/antd/table/tools/filterers.js
+++ src/antd/table/tools/filterers.js
@@ -10,3 +10,20 @@ export const onStringFilter = (v, r, f) =>
     r[f].toLowerCase().includes(v.toLowerCase())
   :
     r[f].length === 0;
+
+/**
+ * Indicates if a record matches a filter value for a given property name. The record property is supposed to be an array of strings
+ * @param {any} v Filter value
+ * @param {object} r Record
+ * @param {string} f Record property name
+ * @returns {boolean}
+ */
+export const onArrayFilter = (v, r, f) => {
+  if (v.length === 0)
+    return r[f].length === 0;
+  else
+    for(const value of r[f])
+      if (value.toLowerCase() === v.toLowerCase())
+        return true;
+  return false;
+}
--- types/antd/table/tools/filterers.d.ts
+++ types/antd/table/tools/filterers.d.ts
@@ -1,2 +1,3 @@
 export function onStringFilter(v: any, r: object, f: string): boolean;
+export function onArrayFilter(v: any, r: object, f: string): boolean;
 //# sourceMappingURL=filterers.d.ts.map
