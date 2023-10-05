29a817f9a1e3a0848901e012987636bac5f59834
PascalPlantey <59055046+PascalPlantey@users.noreply.github.com>
2023-10-05 14:22:31 +0200

    JDocs improvements

--- src/antd/select/tools/buildTreeSelectOptions.js
+++ src/antd/select/tools/buildTreeSelectOptions.js
@@ -3,7 +3,8 @@ import { Compound } from "../../../js";
 /**
  * Build the options array for a tree Select. The source is an array like [[parent1, [child1, child2]], ...]
  * @param {array} source Data source
- * @param {boolean} compoundChildren If true the resulting children values will be compounds "parent|child"
+ * @param {string} labelName Used for the label property name (text in a Table filter, title in a TreeSelect)
+ * @param {boolean} [compoundChildren=true] If true the resulting children values will be compounds "parent|child"
  * @returns {array<array>} [{ text, value, children: [{ text, value }, ...]}, ...]
  * @memberof AntdUtils
  */
--- types/antd/select/tools/buildTreeSelectOptions.d.ts
+++ types/antd/select/tools/buildTreeSelectOptions.d.ts
@@ -2,9 +2,10 @@ export default buildTreeSelectOptions;
 /**
  * Build the options array for a tree Select. The source is an array like [[parent1, [child1, child2]], ...]
  * @param {array} source Data source
- * @param {boolean} compoundChildren If true the resulting children values will be compounds "parent|child"
+ * @param {string} labelName Used for the label property name (text in a Table filter, title in a TreeSelect)
+ * @param {boolean} [compoundChildren=true] If true the resulting children values will be compounds "parent|child"
  * @returns {array<array>} [{ text, value, children: [{ text, value }, ...]}, ...]
  * @memberof AntdUtils
  */
-declare function buildTreeSelectOptions(source: array, labelName: any, compoundChildren?: boolean): array<array>;
+declare function buildTreeSelectOptions(source: array, labelName: string, compoundChildren?: boolean | undefined): array<array>;
 //# sourceMappingURL=buildTreeSelectOptions.d.ts.map
