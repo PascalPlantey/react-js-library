67765f25ea5941cea522bc71b0321616b0cbd42e
PascalPlantey <59055046+PascalPlantey@users.noreply.github.com>
2023-10-06 11:02:10 +0200

    Renamed TupleMap to Tuple_Any

--- src/js/misc/Tuple_Any.js
+++ src/js/misc/Tuple_Any.js
@@ -7,12 +7,12 @@ import Compound from "./Compound";
  * like ['n1', 'n2'] associated with a value
  * @extends ExtMap
  */
-class TupleMap extends ExtMap {
+class Tuple_Any extends ExtMap {
   /**
-   * Builds a new TupleMap 
+   * Builds a new Tuple_Any 
    * @param {any} iterable (see [Mozilla]{@link https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Map/Map})
    * @example
-   * const tuples = new TupleMap([[['Cert',' adm'], 1], [['Cert', 'sales'], 2]]);
+   * const tuples = new Tuple_Any([[['Cert',' adm'], 1], [['Cert', 'sales'], 2]]);
    */
   constructor(iterable) {
     const tuples = [];
@@ -30,11 +30,11 @@ class TupleMap extends ExtMap {
   }
 
   /**
-   * Indicates if the TupleMap own a given key
+   * Indicates if the Tuple_Any own a given key
    * @param  {...any} args Key(s) of the Tuple
    * @returns {boolean}
    * @example
-   * const tuples = new TupleMap([[['Cert',' adm'], 1], [['Cert', 'sales'], 2]]);
+   * const tuples = new Tuple_Any([[['Cert',' adm'], 1], [['Cert', 'sales'], 2]]);
    * tuples.has(['Cert',' adm']);         // => true
    * tuples.has('Cert',' adm');           // => true
    * tuples.has('Cert','adm');            // => true
@@ -49,7 +49,7 @@ class TupleMap extends ExtMap {
    * @param  {...any} args Key(s) of the Tuple
    * @returns {any}
    * @example
-   * const tuples = new TupleMap([[['Cert',' adm'], 1], [['Cert', 'sales'], 2]]);
+   * const tuples = new Tuple_Any([[['Cert',' adm'], 1], [['Cert', 'sales'], 2]]);
    * tuples.get(['Cert',' adm']);         // => 1
    * tuples.has('Cert',' adm');           // => 1
    * tuples.has('Cert', 'service');       // => undefined
@@ -59,7 +59,7 @@ class TupleMap extends ExtMap {
   }
 
   /**
-   * Deletes an element from the TupleMap
+   * Deletes an element from the Tuple_Any
    * @param  {...any} args Keys(s) of the Tuple
    * @returns {boolean} True if an element has been deleted
    */
@@ -83,28 +83,28 @@ class TupleMap extends ExtMap {
    * @param  {...any} args Keys(s) of the Tuple and default value (last argument in the list)
    * @returns {any} Value associated with the key(s)
    * @example
-   * const tuples = new TupleMap([[['Cert',' adm'], 1], [['Cert', 'sales'], 2]]);
+   * const tuples = new Tuple_Any([[['Cert',' adm'], 1], [['Cert', 'sales'], 2]]);
    * tuples.getOrSet('Cert', 'service', 0);               // ==> 0 Adds the tuple ['Cert', 'service'] with value 0
    * tuples.getOrSet('Cert', 'service', 1);               // ==> 0 Return current value of Tuple ['Cert', 'service']
    * tuples.getOrSet(['Cert', 'service'], 1);             // ==> 0 Return current value of Tuple ['Cert', 'service']
    */
   getOrSet(...args) {
-    const [keys, value] = TupleMap.splitArgs(args);
+    const [keys, value] = Tuple_Any.splitArgs(args);
     return super.getOrSet(keys, value);
   }
 
   /**
-   * Set the value of a Tuple, add the Tuple if it does not exist in the TupleMap
+   * Set the value of a Tuple, add the Tuple if it does not exist in the Tuple_Any
    * @param  {...any} args Keys(s) of the Tuple and default value (last argument in the list)
-   * @returns {TupleMap} Current object
+   * @returns {Tuple_Any} Current object
    * @example
-   * const tuples = new TupleMap([[['Cert',' adm'], 1], [['Cert', 'sales'], 2]]);
+   * const tuples = new Tuple_Any([[['Cert',' adm'], 1], [['Cert', 'sales'], 2]]);
    * tuples.set('Cert', 'service', 0);
    * tuples.set('Cert', 'sales', 1);
    * tuples.set(['Cert', 'service'], 1);          // Change value of Tuple ['Cert', 'service']
    */
   set(...args) {
-    const [keys, value] = TupleMap.splitArgs(args)
+    const [keys, value] = Tuple_Any.splitArgs(args)
     return super.set(Compound.structure(keys), value);
   }
 
@@ -114,7 +114,7 @@ class TupleMap extends ExtMap {
    * Mimic the Map.forEach() but using destructured keys
    * @param {function} fn Function applied to each Tuple element
    * @example
-   * const tuples = new TupleMap([[['Cert',' adm'], 1], [['Cert', 'sales'], 2]]);
+   * const tuples = new Tuple_Any([[['Cert',' adm'], 1], [['Cert', 'sales'], 2]]);
    * tuples.tupleForEach((value, key) => console.log('key', key, 'value', value)); // Where key is an array 
    */
   tupleForEach(fn) {
@@ -125,7 +125,7 @@ class TupleMap extends ExtMap {
    * Mimic the Map.keys() but returning destructured keys
    * @generator
    * @example
-   * const tuples = new TupleMap([[['Cert',' adm'], 1], [['Cert', 'sales'], 2]]);
+   * const tuples = new Tuple_Any([[['Cert',' adm'], 1], [['Cert', 'sales'], 2]]);
    * for(const key of tuples.tupleKeys)
    *   console.log('key', key);              // => logs ['Cert', 'adm'] and ['Cert', 'sales']
    */
@@ -138,7 +138,7 @@ class TupleMap extends ExtMap {
    * Mimic the Map.entries() but returning destructured keys
    * @generator
    * @example
-   * const tuples = new TupleMap([[['Cert',' adm'], 1], [['Cert', 'sales'], 2]]);
+   * const tuples = new Tuple_Any([[['Cert',' adm'], 1], [['Cert', 'sales'], 2]]);
    * for(const [[category, certification], value] of tuples.tupleEntries()) {
        console.log('category', category, 'certification', certification, 'value', value);
    * }
@@ -153,8 +153,8 @@ class TupleMap extends ExtMap {
    * @returns {string}
    */
   get [Symbol.toStringTag]() {
-    return 'TupleMap';
+    return 'Tuple_Any';
   }
 };
 
-export default TupleMap;
+export default Tuple_Any;
--- types/js/misc/index.d.ts
+++ types/js/misc/index.d.ts
@@ -3,5 +3,5 @@ export { default as ItemCounters } from "./ItemsCounter";
 export { default as toIterable } from "./toIterable";
 export { default as PropertyValues } from "./PropertyValues";
 export { default as Stats } from "./Stats";
-export { default as TupleMap } from "./TupleMap";
+export { default as Tuple_Any } from "./Tuple_Any";
 //# sourceMappingURL=index.d.ts.map
