3c6c01d974e7bc62ca042d7e3665e09a678655fa
PascalPlantey <59055046+PascalPlantey@users.noreply.github.com>
2024-01-30 12:38:57 +0100

    Added useCheckChanges comment

--- src/hooks/cycles/useRenderCount.js
+++ src/hooks/cycles/useRenderCount.js
@@ -3,12 +3,6 @@ import { useRef } from "react";
 /**
  * Returns the number of renders for the component
  * @returns {number}
- * @example
- * const [obj, setObj] = useState({ a: 1, b: [10, 2] })
- * const [changed, what] = useCheckChanges(obj, obj.values()) // Track changes of obj and its values
- * 
- * useEffect(() => { setObj({ a: 1, b: [12, 3] })}, []) // obj and obj.b did change
- * useEffect(() => { obj.a = 2; setObj(obj); }, [])     // obj.a did change (but no render, [changed, what] not updated)
  */
 const useRenderCount = () => {
   const count = useRef(0);
